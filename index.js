833b4059c523e80eae0a14cf343338e13dde0cb1
zeleffoff <raphaelamoro330@gmail.com>
2023-02-23 10:38:57 +0100

    Radio feature added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Level = require('./src/features/Level');
 const Paginate = require('./src/features/Paginate');
+const Radio = require('./src/features/Radio');
 
 module.exports = {
     Level,
-    Paginate
+    Paginate,
+    Radio
 };
