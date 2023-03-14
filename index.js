c25688d06df9397a46620dcb80155e5fd86d87fb
zeleffoff <raphaelamoro330@gmail.com>
2023-03-14 13:38:34 +0100

    Adding TicTacToe feature

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Level = require('./src/features/Level');
 const Paginate = require('./src/features/Paginate');
 const Radio = require('./src/features/Radio');
+const TicTacToe = require('./src/features/TicTacToe');
 
 module.exports = {
     Level,
     Paginate,
-    Radio
+    Radio,
+    TicTacToe
 };
